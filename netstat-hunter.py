34c34a078fb70b83463dec479eea1bf8841c15a4
karilaf <karinamalik2212@gmail.com>
2023-05-01 19:35:18 -0300

    Melhorada a quantidade de funções e a lógica

--- netstat-hunter.py
+++ netstat-hunter.py
@@ -6,32 +6,29 @@ print(" /  \/ / _ \ __/ __| __/ _` | __| | '_ \| | | | '_ \| __/ _ \ '__|")
 print("/ /\  /  __/ |_\__ \ || (_| | |_  | | | | |_| | | | | ||  __/ |   ")
 print("\_\ \/ \___|\__|___/\__\__,_|\__| |_| |_|\__,_|_| |_|\__\___|_|   ")
 
-def config():
+def config(api_key):
     # Lógica para configurar a API aqui
     print("Configurar API selecionada")
 
-def portas():
-    # Lógica para configurar as portas aqui
-    print("Configurar Portas selecionado")
-
 def execute():
-    # Lógica para executar aqui
+    netstat()
     print("Executar selecionado")
+
+def netstat():
+    # Função para rodar o netstat e retornar a outra função x os IPs para que seja feita a chamada da API
+    pass
 
 # Define os argumentos de linha de comando
 parser = argparse.ArgumentParser(description='Menu - Netstat Hunter')
-parser.add_argument('--config', action='store_true', help='Configura a API')
-parser.add_argument('--portas', action='store_true', help='Configura as portas')
+parser.add_argument('--config', dest='api_key', help='Chave da API para configuração')
 parser.add_argument('--execute', action='store_true', help='Executa')
 
 # Analisa os argumentos de linha de comando
 args = parser.parse_args()
 
-# Chama a função correspondente ao argumento de linha de comando
-if args.config:
-    config()
-elif args.portas:
-    portas()
+# Chama a função correspondente ao argumento de linha de linha de comando
+if args.api_key:
+    config(args.api_key)
 elif args.execute:
     execute()
 else:
