ed62cf916442d4c347f7c5df48ac4c508b4d028e
karilaf <karinamalik2212@gmail.com>
2023-05-02 22:40:51 -0300

    Corrigido bug da chave de api não ficar no buffer

--- netstat-hunter.py
+++ netstat-hunter.py
@@ -1,8 +1,8 @@
-import argparse
 import time
 import requests
 import re
 import subprocess
+import argparse
 
 print("     __     _       _        _     _                 _            ")
 print("  /\ \ \___| |_ ___| |_ __ _| |_  | |__  _   _ _ __ | |_ ___ _ __ ")
@@ -10,11 +10,22 @@ print(" /  \/ / _ \ __/ __| __/ _` | __| | '_ \| | | | '_ \| __/ _ \ '__|")
 print("/ /\  /  __/ |_\__ \ || (_| | |_  | | | | |_| | | | | ||  __/ |   ")
 print("\_\ \/ \___|\__|___/\__\__,_|\__| |_| |_|\__,_|_| |_|\__\___|_|   ")
 
-api_key = '' # Variável global
+API_KEY_BUFFER = ""
+LAST_UPDATE = 0
 
 def config(ip):
-    global api_key # Avisa que a variável será usada no escopo global
-    # Lógica para configurar a API aqui
+    global API_KEY_BUFFER, LAST_UPDATE
+
+    # Se o buffer não está vazio e já se passou menos de 30 minutos desde a última atualização, utiliza o valor armazenado
+    if API_KEY_BUFFER != "" and time.time() - LAST_UPDATE < 1800:
+        api_key = API_KEY_BUFFER
+    else:
+        # Se não, solicita a chave de API ao usuário
+        api_key = input("Digite a chave de API: ")
+        API_KEY_BUFFER = api_key
+        LAST_UPDATE = time.time()
+
+    # Faz a consulta do IP utilizando a API
     url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
     params = {'apikey': api_key, 'ip': ip}
     response = requests.get(url, params=params)
@@ -27,7 +38,8 @@ def config(ip):
         print('Erro ao fazer a consulta de IP')
     
     time.sleep(15) # Atraso de 15 segundos entre cada chamada
-def execute():
+
+def execute(api_key):
     non_internal_ips = netstat()
     for ip in non_internal_ips:
         config(ip)
@@ -49,19 +61,17 @@ def netstat():
 #--------------------------------------------------
 # Define os argumentos de linha de comando
 parser = argparse.ArgumentParser(description='Menu - Netstat Hunter')
-parser.add_argument('--config', dest='api_key', help='Chave da API para configuração')
-parser.add_argument('--execute', action='store_true', help='Executa')
+parser.add_argument('--key', dest='api_key', help='Chave da API para configuração e execução')
 
 # Analisa os argumentos de linha de comando
 args = parser.parse_args()
 
-# Define o valor da variável global 'api_key'
+# Define o valor da variável global 'API_KEY_BUFFER'
 if args.api_key:
-    api_key = args.api_key
+    API_KEY_BUFFER = args.api_key
 
 # Chama a função correspondente ao argumento de linha de linha de comando
-if args.execute:
-    execute()
-elif args.api_key:
-    print("É necessário utilizar a opção --execute para executar a busca de IPs.")
-
+if args.api_key:
+    execute(args.api_key)
+else:
+    print("É necessário utilizar a opção --key para informar a chave de API.")
